438b416e9a01977cba99dfa0ef137c27c500cd9d
Tyler Eason <easontm@gmail.com>
2025-11-13 00:19:15 +0900

    precommit, parameterize provider, invalidate old token

--- infra/modules/providers.tf
+++ infra/modules/providers.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.10.6"
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
@@ -9,16 +10,9 @@ terraform {
 
 
 
-# provider "proxmox" {
-#   pm_api_url      = var.proxmox_api_url
-#   pm_api_token_id = var.proxmox_api_token_id
-#   pm_api_token_secret = var.proxmox_api_token_secret
-#   pm_tls_insecure = true
-# }
-
 provider "proxmox" {
-  pm_api_url      = "https://192.168.1.203:8006/api2/json"
-  pm_api_token_id = "terraform-prov@pve!iac-token"
-  pm_api_token_secret = "15327108-8272-43e3-a140-d9308851f3f1"
-  pm_tls_insecure = true
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true
 }

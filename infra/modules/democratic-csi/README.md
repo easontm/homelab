fd84d3c3a00714b8e8a26c965d73057db9b6fbc0
Tyler Eason <easontm@gmail.com>
2026-01-02 23:44:50 +0900

    Adding iSCSI Block storage support from TrueNAS to k8s (#8)

---
+++ infra/modules/democratic-csi/README.md
@@ -0,0 +1,26 @@
+# democratic-csi
+
+[democratic-csi on GitHub](https://github.com/democratic-csi/democratic-csi)
+
+## Setup
+
+1. On TrueNAS, create the iSCSI datasets to store snapshots and volumes.
+2. Create an API key for the CSI controller to use.
+   1. To be honest I searched around for the privilege list but couldn't find it,
+   and the list seems pretty long. So I'm using an admin token. Has some risk but
+   I accept it.
+3. On each of the k8s nodes, run the following:
+    ```bash
+    sudo apt install nfs-common open-iscsi multipath-tools scsitools lsscsi
+    sudo tee /etc/multipath.conf >/dev/null <<EOF
+    defaults {
+        user_friendly_names yes
+        find_multipaths yes
+    }
+    EOF
+    # Save the output of this step
+    cat /etc/iscsi/initiatorname.iscsi
+    ```
+4. On TrueNAS, add the saved Initiator Names from the k8s nodes to the 
+   initiator list. (`Shares` -> `Block (iSCSI) Shares Targets` -> `Initiators`)
+5. Apply the module
---
+++ infra/modules/democratic-csi/test_pod.tf
@@ -0,0 +1,58 @@
+resource "kubernetes_manifest" "smoke_test_pvc" {
+  count      = var.test ? 1 : 0
+  depends_on = [helm_release.democratic_csi]
+  manifest = {
+    apiVersion = "v1"
+    kind       = "PersistentVolumeClaim"
+    metadata = {
+      name      = "iscsi-test-pvc"
+      namespace = "default"
+    }
+    spec = {
+      storageClassName = var.storage_class_name
+      accessModes      = ["ReadWriteOnce"]
+      resources = {
+        requests = {
+          storage = "100Mi"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_pod_v1" "smoke_test_pod" {
+  count      = var.test ? 1 : 0
+  depends_on = [kubernetes_manifest.smoke_test_pvc]
+  metadata {
+    name      = "iscsi-test-pod"
+    namespace = "default"
+  }
+
+  spec {
+    container {
+      name  = "iscsi-test-container"
+      image = "busybox"
+
+      command = [
+        "sh",
+        "-c",
+        "echo ok > /data/test && sleep 30"
+      ]
+
+      volume_mount {
+        name       = "data"
+        mount_path = "/data"
+      }
+    }
+
+    volume {
+      name = "data"
+
+      persistent_volume_claim {
+        claim_name = "iscsi-test-pvc"
+      }
+    }
+
+    restart_policy = "Never"
+  }
+}

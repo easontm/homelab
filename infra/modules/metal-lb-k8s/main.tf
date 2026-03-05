5baa8ee8d88a97b2274e4a7b9ac71d3e835a05b3
Tyler Eason <easontm@gmail.com>
2026-03-05 21:49:31 +0900

    metallb update

--- infra/modules/metal-lb-k8s/main.tf
+++ infra/modules/metal-lb-k8s/main.tf
@@ -9,17 +9,31 @@ provider "kubernetes" {
 }
 
 
+resource "kubernetes_namespace_v1" "metallb" {
+  metadata {
+    name = var.namespace
+
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
 resource "helm_release" "metallb" {
+  depends_on       = [kubernetes_namespace_v1.metallb]
   name             = "metallb"
   repository       = "https://metallb.github.io/metallb"
   chart            = "metallb"
   namespace        = var.namespace
   create_namespace = false
   version          = var.chart_version
-  timeout          = 60
+  timeout          = 180
 }
 
 resource "kubernetes_manifest" "ip_address_pool" {
+  depends_on = [helm_release.metallb]
   manifest = {
     apiVersion = "metallb.io/v1beta1"
     kind       = "IPAddressPool"
@@ -34,6 +48,7 @@ resource "kubernetes_manifest" "ip_address_pool" {
 }
 
 resource "kubernetes_manifest" "l2_advertisement" {
+  depends_on = [helm_release.metallb]
   manifest = {
     apiVersion = "metallb.io/v1beta1"
     kind       = "L2Advertisement"
